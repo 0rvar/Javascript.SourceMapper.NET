7abda9e039273f025c727c4b6c4fdc70b65ea4ed
Orvar Segerström <orvarsegerstrom@gmail.com>
2016-06-13 22:46:01 +0200

    Implement FFI

---
+++ Javascript.SourceMapper.Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Javascript.SourceMapper.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = "{ \"version\": 3, \"file\": \"foo.js\", \"sources\": [\"source.js\"], \"names\": [\"name1\", \"name1\", \"name3\"], \"mappings\": \";EAACA;;IAEEA;;MAEEE\", \"sourceRoot\": \"http://example.com\" }";
+            var cache = new SourceMapCache(json);
+
+            var mapping = cache.SourceMappingFor(2, 2);
+
+            Console.WriteLine("{0}", mapping);
+        }
+    }
+}
---
+++ Javascript.SourceMapper/SourceMapCache.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Javascript.SourceMapper
+{
+    internal class RustFFIApi
+    {
+        [DllImport("JsSourceMapper_FFI", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern CacheHandle cache_init(string json);
+        [DllImport("JsSourceMapper_FFI", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void cache_free(IntPtr cache);
+
+        [DllImport("JsSourceMapper_FFI", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern string get_error(CacheHandle cache);
+        [DllImport("JsSourceMapper_FFI", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void error_free(string error);
+
+        [DllImport("JsSourceMapper_FFI", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern FFIMapping find_mapping(CacheHandle cache, uint line, uint column);
+        [DllImport("JsSourceMapper_FFI", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern uint mapping_free(FFIMapping mapping);
+    }
+
+    internal class CacheHandle : SafeHandle
+    {
+        public CacheHandle() : base(IntPtr.Zero, true) { }
+
+        public override bool IsInvalid
+        {
+            get { return false; }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            RustFFIApi.cache_free(handle);
+            return true;
+        }
+    }
+
+    public class SourceMapParsingException : Exception
+    {
+        public SourceMapParsingException(string message) : base(message) { }
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    internal struct FFIMapping
+    {
+        public uint source_line;
+        public uint source_column;
+        public uint generated_line;
+        public uint generated_column;
+        public IntPtr source;
+        public IntPtr name;
+    }
+
+    public class SourceMapping
+    {
+        public uint SourceLine;
+        public uint SourceColumn;
+        public uint GeneratedLine;
+        public uint GeneratedColumn;
+        public string Source;
+        public string Name;
+    }
+
+    public class SourceMapCache : IDisposable
+    {
+        private CacheHandle cache;
+
+        public SourceMapCache(string json)
+        {
+            cache = RustFFIApi.cache_init(json);
+            string temp = RustFFIApi.get_error(cache);
+            if(temp != null)
+            {
+                string error = (string)temp.Clone();
+                RustFFIApi.error_free(temp);
+                throw new SourceMapParsingException(error);
+            }
+        }
+
+        public SourceMapping SourceMappingFor(uint line, uint column)
+        {
+            FFIMapping ffiMapping = RustFFIApi.find_mapping(cache, line, column);
+            var mapping = new SourceMapping
+            {
+                SourceLine = ffiMapping.source_line,
+                SourceColumn = ffiMapping.source_column,
+                GeneratedLine = ffiMapping.generated_line,
+                GeneratedColumn = ffiMapping.generated_column,
+                Source = (string)Marshal.PtrToStringUni(ffiMapping.source).Clone(),
+                Name = (string)Marshal.PtrToStringUni(ffiMapping.name).Clone()
+            };
+            RustFFIApi.mapping_free(ffiMapping);
+            return mapping;
+        }
+
+        public void Dispose()
+        {
+            cache.Dispose();
+        }
+    }
+
+}
